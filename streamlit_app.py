c494c72dd687e9bf0b6b1a43a8a4f0d576c18ca5
davefrau <134781586+davefrau@users.noreply.github.com>
2023-05-27 08:03:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title("My Parents new healthy Diner")
+streamlit.header("Breakfast")
+streamlit.text("Yogurt, Blueberries and Granola")
+streamlit.text("Egg White Omelete")
