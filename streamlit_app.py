a5c75c15e94ca947ab11ccf863d2341ea3526160
davefrau <134781586+davefrau@users.noreply.github.com>
2023-05-27 20:06:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
+#imports 
 import streamlit
 import pandas 
 import requests 
+import snowflake.connector 
+from urllib.error import URLError
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.title("My Parents new healthy Diner")
@@ -27,8 +31,10 @@ fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fru
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+#don't crun anything past here while we troubeshoot
+streamlit.stop()
+
 #Winding down the lesson importing python
-import snowflake.connector 
 
 #testing the python from streamlit 
 
